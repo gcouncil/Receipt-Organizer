5419f617dc992aa2e061134221c140d60de88e1c
Hargobind Khalsa <hargobind@quickleft.com>
2014-04-01 14:09:21 -0600

    Get scanner to cooperate with e2e tests, move to uuids

--- migrations/20140331095115_create_images.js
+++ migrations/20140331095115_create_images.js
@@ -1,22 +1,33 @@
+var async = require('async');
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('images', function(t) {
-    t.uuid('uuid').primary().defaultTo('uuid_generate_v1()');
-    t.text('data');
-    t.text('url');
+  return Promise.promisify(async.series)([
+    function(cb) {
+      knex.schema.createTable('images', function(t) {
+        t.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v1()'));
+        t.text('data');
+        t.text('url');
 
-    t.timestamps();
-  }).then(function() {
-    return knex.schema.table('receipts', function(t) {
-      t.uuid('image').references('uuid').inTable('images');
-    });
-  });
+        t.timestamps();
+      }).exec(cb);
+    },
+    function(cb) {
+      knex.schema.table('receipts', function(t) {
+        t.uuid('image').references('id').inTable('images');
+      }).exec(cb);
+    }
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.table('receipts', function(t) {
-    t.dropColumn('image');
-  }).then(function() {
-    return knex.schema.dropTable('images');
-  });
+  return Promise.promisify(async.series)([
+    function(cb) {
+      knex.schema.table('receipts', function(t) {
+        t.dropColumn('image');
+      }).exec(cb);
+    },
+    function(cb) {
+      return knex.schema.dropTable('images').exec(cb);
+    }
+  ]);
 };
