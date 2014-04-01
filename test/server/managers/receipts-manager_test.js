91263d7c4c66d43a4f606749cb0882ed6ab6449b
Ben Lewis <bennlewis@gmail.com>
2014-04-01 12:54:27 -0600

    add timeout to flash messages, and contrain them to only one at a time

--- test/server/managers/receipts-manager_test.js
+++
@@ -1,9 +0,0 @@
-//var manager = require('../../app/managers/receipts-manager');
-
-//var helpers = require('../test-helper');
-//var expect = helpers.expect;
-
-describe('ReceiptsManager', function() {
-  describe('#query', function() {
-  });
-});
