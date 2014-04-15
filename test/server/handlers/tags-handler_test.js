5e83639f604914e3bc4ed62f082888366c76bf08
Ben Lewis <bennlewis@gmail.com>
2014-04-15 13:00:50 -0600

    create tags handler test for update

--- test/server/handlers/tags-handler_test.js
+++ test/server/handlers/tags-handler_test.js
@@ -109,6 +109,54 @@ describe('TagsHandler', function() {
     });
   });
 
+  describe('update', function() {
+    context('with existing tags', function() {
+      beforeEach(function(done) {
+        var self = this;
+
+        this.manager = {
+          update: this.sinon.stub().callsArgWith(3, null, [
+            new domain.Tag({
+              name: 'Travel'
+            })
+          ])
+        };
+
+        var user = {
+          id: '1a2b3c',
+          email: 'abc@abc.com',
+          token: 'XYZ'
+        };
+
+        var app = express();
+        app.use(login(user));
+        app.use(require('body-parser')());
+        app.use('/:tag', handler(this.manager).update);
+
+        request(app)
+        .put('/UUID')
+        .send({ name: 'Travel' })
+        .end(function(err, res) {
+          self.res = res;
+          done(err);
+        });
+      });
+
+      it('should return an HTTP 200', function() {
+        expect(this.res.status).to.equal(200);
+      });
+
+      it('should respond with the updated receipt', function() {
+        expect(this.res.body).to.have.deep.property('[0].name', 'Travel');
+      });
+
+      it('should pass the new attributes to the manager', function() {
+        expect(this.manager.update).to.have.been.calledWith('UUID', { name: 'Travel'}, { 'user': '1a2b3c'}, sinon.match.func);
+      });
+    });
+  });
+
+
   describe('destroy', function() {
     context('with existing tags', function() {
       beforeEach(function(done) {
