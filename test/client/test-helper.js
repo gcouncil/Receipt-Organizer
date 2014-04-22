4fcd591e455cb0bb593ea3f7edce333a41132d41
rrgayhart <rrgayhart@gmail.com>
2014-04-22 11:17:43 -0600

    Moves helper logic out of index

--- test/client/test-helper.js
+++ test/client/test-helper.js
@@ -2,3 +2,15 @@ require('epson-receipts/client');
 require('angular-mocks');
 
 var angular = require('angular');
+
+require('chai').use(require('sinon-chai'));
+
+var sinon = require('sinon');
+
+beforeEach(function() {
+  this.sinon = sinon.sandbox.create();
+});
+
+afterEach(function() {
+  this.sinon.restore();
+});
