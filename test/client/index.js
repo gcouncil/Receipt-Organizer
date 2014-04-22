57ac26091fc737f5fca75e4ddfedd02b4f09e50d
rrgayhart <rrgayhart@gmail.com>
2014-04-22 11:14:06 -0600

    Adds simon and tests bulk selection

--- test/client/index.js
+++ test/client/index.js
@@ -3,3 +3,15 @@ require('angular-mocks');
 
 require('./widgets');
 require('./imageviewer');
+
+require('chai').use(require('sinon-chai'));
+
+var sinon = require('sinon');
+
+beforeEach(function() {
+  this.sinon = sinon.sandbox.create();
+});
+
+afterEach(function() {
+  this.sinon.restore();
+});
