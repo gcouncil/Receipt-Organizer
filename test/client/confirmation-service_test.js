dccc973676c39d16ffc5ccf0d8187fe83a735388
rrgayhart <rrgayhart@gmail.com>
2014-04-24 15:54:43 -0600

    Completes confirmation service test
    with @fluxusfrequency

--- test/client/confirmation-service_test.js
+++ test/client/confirmation-service_test.js
@@ -11,30 +11,59 @@ describe('Confirmation', function() {
           $scope: {},
           destroy: ctx.sinon.stub()
         };
-
         return ctx.modal;
       }
     });
   });
 
-  beforeEach(angular.mock.inject(function(confirmation) {
-    this.confirmation = confirmation;
-  }));
-
-  it('responds by destroying the dialog upon receiving yes or no', function(done) {
+  it('responds by destroying the dialog upon receiving yes', function(done) {
     var ctx = this;
+    angular.mock.inject(function(confirmation, $rootScope) {
+      var promise = confirmation({});
+      ctx.modal.$scope.yes();
 
-    var promise = this.confirmation({});
-    ctx.modal.$scope.yes();
+      expect(ctx.modal.destroy).to.have.been.called;
+      expect(promise).notify(done);
 
-    expect(ctx.modal.destroy).to.have.been.called;
-    promise.then(function() { done(); }, done);
+      $rootScope.$digest();
+    });
   });
 
-  it('responds with deferred.resolve on receiving a yes', function() {
+  it('responds by destroying the dialog upon receiving no', function(done) {
+    var ctx = this;
+    angular.mock.inject(function(confirmation, $rootScope) {
+      var promise = confirmation({});
+      ctx.modal.$scope.no();
+
+      expect(ctx.modal.destroy).to.have.been.called;
+      expect(promise).notify(done);
+
+      $rootScope.$digest();
+    });
   });
 
-  it('responds with deferred.destroy on receiving a no', function() {
+  it('responds with deferred.resolve on receiving a yes', function(done) {
+    var ctx = this;
+    angular.mock.inject(function(confirmation, $rootScope) {
+      var promise = confirmation({});
+      ctx.modal.$scope.yes();
+
+      expect(promise).to.be.fulfilled.and.notify(done);
+
+      $rootScope.$digest();
+    });
+  });
+
+  it('responds with deferred.destroy on receiving a no', function(done) {
+    var ctx = this;
+    angular.mock.inject(function(confirmation, $rootScope) {
+      var promise = confirmation({});
+      ctx.modal.$scope.no();
+
+      expect(promise).to.be.rejected.and.notify(done);
+
+      $rootScope.$digest();
+    });
   });
 
 });
