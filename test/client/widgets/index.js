67bc008f2c57ec43563be11901f6153ccb2b7c52
Ben Lewis <bennlewis@gmail.com>
2014-04-28 16:59:35 -0600

    refactor datepicker directive to remove unneccesary ugly hack

--- test/client/widgets/index.js
+++ test/client/widgets/index.js
@@ -1,6 +1,6 @@
-require('./currency-input_test');
-require('./confirmation-input_test');
-require('./is-selected_test');
 require('./bulk-selection_test');
+require('./confirmation-input_test');
+require('./currency-input_test');
 require('./form-group-test');
+require('./is-selected_test');
 require('./table-data-field_test');
