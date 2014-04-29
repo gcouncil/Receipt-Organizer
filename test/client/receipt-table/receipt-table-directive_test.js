7edbce9972cd3c1d16225215139f92b7e32e8815
Ben Lewis <bennlewis@gmail.com>
2014-04-29 17:04:40 -0600

    receipt table directive test

--- test/client/receipt-table/receipt-table-directive_test.js
+++ test/client/receipt-table/receipt-table-directive_test.js
@@ -1,7 +1,8 @@
 var angular = require('angular');
 var expect = require('chai').expect;
+var $ = require('jquery');
 
-describe.only('receipt table directive', function() {
+describe('receipt table directive', function() {
 
   beforeEach(function() {
     var ctx = this;
@@ -16,10 +17,25 @@ describe.only('receipt table directive', function() {
 
       ctx.compile = function() {
         ctx.element = $compile('<receipt-table></receipt-table>')(ctx.scope);
+        ctx.scope.receipts = [{
+          vendor: 'a',
+          date: new Date(),
+          category: 'b',
+          paymentType: 'c',
+          city: 'd'
+        }];
         ctx.scope.$digest();
       };
     });
+
     ctx.compile();
+
+    ctx.event = {
+      target: ctx.element.find('td')[0],
+      which: 0,
+      preventDefault: ctx.sinon.stub()
+    };
+
   });
 
   afterEach(function() {
@@ -29,7 +45,91 @@ describe.only('receipt table directive', function() {
     }
   });
 
-  it('', function() {
-    expect(true).to.be.true
+  describe('keydown events', function() {
+    beforeEach(function() {
+      var ctx = this;
+
+      ctx.sinon.stub($.fn, 'focus');
+
+      ctx.triggerKeydown = function() {
+        var e = $.Event('keydown', ctx.event);
+        $(ctx.element).trigger(e);
+      };
+
+      ctx.itShouldFocus = function() {
+        expect($.fn.focus).to.have.been.called;
+      };
+
+      ctx.itShouldNotFocus = function() {
+        expect($.fn.focus).not.to.have.been.called;
+      };
+
+      ctx.itShouldPreventDefault = function() {
+        expect(ctx.event.preventDefault).to.have.been.called;
+      };
+
+      ctx.itShouldNotPreventDefault = function() {
+        expect(ctx.event.preventDefault).not.to.have.been.called;
+      };
+    });
+
+    it('should work with enter key', function() {
+      var ctx = this;
+      ctx.event.which = 13;
+      ctx.triggerKeydown();
+      ctx.itShouldPreventDefault();
+      ctx.itShouldFocus();
+    });
+
+    it('should work with shift+enter key', function() {
+      var ctx = this;
+      ctx.event.shiftKey = true;
+      ctx.event.which = 13;
+      ctx.triggerKeydown();
+      ctx.itShouldPreventDefault();
+      ctx.itShouldFocus();
+    });
+
+    it('should work with ctl+left key', function() {
+      var ctx = this;
+      ctx.event.ctrlKey = true;
+      ctx.event.which = 37;
+      ctx.triggerKeydown();
+      ctx.itShouldPreventDefault();
+      ctx.itShouldFocus();
+    });
+
+    it('should work with ctl+right key', function() {
+      var ctx = this;
+      ctx.event.ctrlKey = true;
+      ctx.event.which = 39;
+      ctx.triggerKeydown();
+      ctx.itShouldPreventDefault();
+      ctx.itShouldFocus();
+    });
+
+    it('should work with up key', function() {
+      var ctx = this;
+      ctx.event.which = 38;
+      ctx.triggerKeydown();
+      ctx.itShouldPreventDefault();
+      ctx.itShouldFocus();
+    });
+
+    it('should work with down key', function() {
+      var ctx = this;
+      ctx.event.which = 40;
+      ctx.triggerKeydown();
+      ctx.itShouldPreventDefault();
+      ctx.itShouldFocus();
+    });
+
+    it('should not work with random keys', function() {
+      var ctx = this;
+      ctx.event.which = 1;
+      ctx.triggerKeydown();
+      ctx.itShouldNotPreventDefault();
+      ctx.itShouldNotFocus();
+    });
   });
 });
