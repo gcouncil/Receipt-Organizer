15e90080cba876586cb21562ab5563edf659db08
Hargobind Khalsa <hargobind@quickleft.com>
2014-04-29 09:37:07 -0600

    Fix small bugs introduced by users-tests branch

--- test/client/users/login/login-form-directive_test.js
+++ test/client/users/login/login-form-directive_test.js
@@ -1,7 +1,7 @@
 var angular = require('angular');
 var expect = require('chai').expect;
 
-describe.only('loginForm directive', function() {
+describe('loginForm directive', function() {
   beforeEach(function() {
     var ctx = this;
 
