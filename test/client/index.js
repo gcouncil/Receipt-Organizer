4176e6ff3003fd1b219b5b5c535f6f5521af4314
Ben Lewis <bennlewis@gmail.com>
2014-04-30 09:54:25 -0600

    create receipt thumbnail grid directive test, WIP

--- test/client/index.js
+++ test/client/index.js
@@ -5,6 +5,7 @@ require('./images');
 require('./layout');
 require('./notify');
 require('./pagination');
+require('./receipt-thumbnail');
 require('./scanning');
 require('./users');
 require('./widgets');
