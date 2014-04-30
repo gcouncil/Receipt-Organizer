daa14e3a7c3370e4577d56c2de10f339edd8d89d
Ben Lewis <bennlewis@gmail.com>
2014-04-30 11:35:45 -0600

    receipts toolbar tagging test

--- test/client/index.js
+++ test/client/index.js
@@ -5,6 +5,7 @@ require('./images');
 require('./layout');
 require('./notify');
 require('./pagination');
+require('./receipts-toolbar');
 require('./scanning');
 require('./users');
 require('./widgets');
