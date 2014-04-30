1bc7785b2d3ddb03c4eef21729c69bc07acf9584
Ben Lewis <bennlewis@gmail.com>
2014-04-30 16:12:30 -0600

    WIP receipt storage test

--- test/client/storage/receipt-storage_test.js
+++ test/client/storage/receipt-storage_test.js
@@ -1,17 +1,48 @@
 var angular = require('angular');
 var expect = require('chai').expect;
 
-describe(' storage service', function() {
+describe.only('receipt storage service', function() {
 
   beforeEach(function() {
     var ctx = this;
-    angular.mock.module('epsonreceipts.storage');
-  });
+    ctx.domain = {
+      Receipt: ctx.sinon.stub()
+    };
 
-  it('', function() {
-    var ctx = this;
-    angular.mock.inject(function( ) {
-      expect(true).to.be.true;
+    angular.mock.module('epsonreceipts.storage', {
+      domain: ctx.domain
     });
   });
+
+  describe('load', function() {
+    it('should get the receipts from the server', function(done) {
+      var ctx = this;
+      angular.mock.inject(function($rootScope, $httpBackend, receiptStorage) {
+        $httpBackend.expectGET('/api/receipts').respond(200, { 1: { id: 1, name: 'RECEIPT1'}, 2: { id: 2, name: 'RECEIPT2' } } );
+
+        var promise = receiptStorage.load();
+
+        $httpBackend.flush();
+        expect(ctx.domain.Receipt).to.have.been.calledTwice.and.to.have.been.calledWithNew;
+        expect(promise).notify(done);
+
+        $rootScope.$digest();
+      });
+    });
+  });
+
+  describe('fetch', function() {
+    it('should fetch the receipt from the server', function(done) {
+      var ctx = this;
+      ctx.receipt = { id: 3, name: 'RECEIPT3' };
+      angular.mock.inject(function($rootScope, $q, receiptStorage) {
+        var promise = receiptStorage.fetch(3);
+
+        expect(promise).to.eventually.equal(ctx.receipt).and.notify(done);
+
+        $rootScope.$digest();
+      });
+    });
+  });
+
 });
