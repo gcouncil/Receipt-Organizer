b08ecb5c3787ae2992215850b193c5d7e1d850ba
Ben Lewis <bennlewis@gmail.com>
2014-05-01 09:21:50 -0600

    WIP receipt storage test

--- test/client/storage/receipt-storage_test.js
+++ test/client/storage/receipt-storage_test.js
@@ -32,10 +32,10 @@ describe.only('receipt storage service', function() {
   });
 
   describe('fetch', function() {
-    it('should fetch the receipt from the server', function(done) {
+    xit('should fetch the receipt', function(done) {
       var ctx = this;
       ctx.receipt = { id: 3, name: 'RECEIPT3' };
-      angular.mock.inject(function($rootScope, $q, receiptStorage) {
+      angular.mock.inject(function($rootScope, receiptStorage) {
         var promise = receiptStorage.fetch(3);
 
         expect(promise).to.eventually.equal(ctx.receipt).and.notify(done);
@@ -44,5 +44,92 @@ describe.only('receipt storage service', function() {
       });
     });
   });
+
+  describe('watch', function() {
+    xit('should watch the receipt collection', function(done) {
+      var ctx = this;
+      angular.mock.inject(function($rootScope, receiptStorage) {
+        receiptStorage.watch();
+      });
+    });
+  });
+
+  describe('create', function() {
+    it('should send a new receipt to the server', function(done) {
+      var ctx = this;
+      ctx.receipt = { id: 4, name: 'RECEIPT4' };
+      angular.mock.inject(function($rootScope, $httpBackend, receiptStorage) {
+        $httpBackend.expectPOST('/api/receipts').respond(201, ctx.receipt);
+
+        ctx.domain.Receipt.returns(ctx.receipt);
+
+        var promise = receiptStorage.create(ctx.receipt);
+
+        $httpBackend.flush();
+        expect(ctx.domain.Receipt).to.have.been.calledWithNew;
+        expect(promise).notify(done);
+
+        $rootScope.$digest();
+      });
+    });
+  });
+
+  describe('update', function() {
+    it('should change the receipt on the server', function(done) {
+      var ctx = this;
+      ctx.receipt = { id: 5, name: 'RECEIPT5' };
+      angular.mock.inject(function($rootScope, $httpBackend, receiptStorage) {
+        $httpBackend.expectPUT('/api/receipts/5').respond(200, ctx.receipt);
+
+        ctx.domain.Receipt.returns(ctx.receipt);
+
+        var promise = receiptStorage.update(ctx.receipt);
+
+        $httpBackend.flush();
+        expect(ctx.domain.Receipt).to.have.been.calledWithNew;
+        expect(promise).to.eventually.equal(ctx.receipt).and.notify(done);
+
+        $rootScope.$digest();
+      });
+    });
+  });
+
+  describe('persist', function() {
+    xit('should create or update the receipt', function(done) {
+      var ctx = this;
+      ctx.receipt = { id: 6, name: 'RECEIPT6' };
+      angular.mock.inject(function($rootScope, $httpBackend, receiptStorage) {
+        $httpBackend.expectPUT('/api/receipts/6').respond(200, ctx.receipt);
+
+        ctx.domain.Receipt.returns(ctx.receipt);
+
+        var promise = receiptStorage.update(ctx.receipt);
+
+        $httpBackend.flush();
+        expect(ctx.domain.Receipt).to.have.been.calledWithNew;
+        expect(promise).to.eventually.equal(ctx.receipt).and.notify(done);
+
+        $rootScope.$digest();
+      });
+    });
+  });
+
+  describe('destroy', function() {
+    it('should destroy the receipt', function(done) {
+      var ctx = this;
+      ctx.receipt = { id: 7, name: 'RECEIPT7' };
+      angular.mock.inject(function($rootScope, $httpBackend, receiptStorage) {
+        $httpBackend.expectDELETE('/api/receipts/7').respond(200);
+
+        var promise = receiptStorage.destroy(ctx.receipt);
+
+        $httpBackend.flush();
+        expect(promise).notify(done);
+
+        $rootScope.$digest();
+      });
+    });
+  });
+
 
 });
