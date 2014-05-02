6f6f6df147b5a670ec5d699a1c552b055dec8714
Ben Lewis <bennlewis@gmail.com>
2014-05-02 09:27:42 -0600

    tag organizer directive

--- test/client/tags/tag-organizer/tag-organizer-directive_test.js
+++ test/client/tags/tag-organizer/tag-organizer-directive_test.js
@@ -1,16 +1,29 @@
 var angular = require('angular');
 var expect = require('chai').expect;
 
-describe('directive', function() {
+describe('tag organizer directive', function() {
   beforeEach(function() {
     var ctx = this;
+    ctx.tagStorage = {
+      query: ctx.sinon.stub(),
+      update: ctx.sinon.stub(),
+      destroy: ctx.sinon.stub()
+    };
+    ctx.state = {
+      state: 'STATE',
+      $current: ctx.sinon.stub(),
+      go: ctx.sinon.stub()
+    };
 
-    angular.mock.module('ngMock', 'epsonreceipts.tags');
+    angular.mock.module('ngMock', 'epsonreceipts.tags', {
+      tagStorage: ctx.tagStorage,
+      $state: ctx.state
+    });
 
     angular.mock.inject(function($rootScope, $compile) {
       ctx.scope = $rootScope.$new();
       ctx.compile = function() {
-        ctx.element = $compile('')(ctx.scope);
+        ctx.element = $compile('<tag-organizer></tag-organizer>')(ctx.scope);
         ctx.scope.$digest();
       };
     });
@@ -24,7 +37,47 @@ describe('directive', function() {
     }
   });
 
-  it('', function() {
-    expect(true).to.be.true;
+  it('should set the scopes state to the state service', function() {
+    var ctx = this;
+    expect(ctx.scope.$state).to.equal(ctx.state);
+  });
+
+  it('should query tagStorage', function() {
+    var ctx = this;
+    ctx.tags = ['TAG1', 'TAG1'];
+    ctx.tagStorage.query.returns(ctx.tags);
+    ctx.scope.$digest();
+    expect(ctx.tagStorage.query).to.have.been.called;
+  });
+
+  it('should delete a tag', function() {
+    var ctx = this;
+    ctx.scope.delete('TAG');
+    ctx.scope.$digest();
+    expect(ctx.tagStorage.destroy).to.have.been.calledWith('TAG');
+  });
+
+  it('should filter tags', function() {
+    var ctx = this;
+    ctx.scope.filter('billable');
+    ctx.scope.$digest();
+    expect(ctx.state.go).to.have.been.calledWith(ctx.state.$current, { tag: 'billable' });
+  });
+
+  it('should update a tag and turn off the edit panel', function() {
+    var ctx = this;
+    ctx.tag = { name: 'TAG', showEdit: true };
+    ctx.scope.update(ctx.tag);
+    ctx.scope.$digest();
+    expect(ctx.tagStorage.update).to.have.been.calledWith(ctx.tag);
+    expect(ctx.tag.showEdit).to.be.false;
+  });
+
+  it('should hide the edit panel', function() {
+    var ctx = this;
+    ctx.tag = { name: 'TAG', showEdit: true };
+    ctx.scope.noEdit(ctx.tag);
+    ctx.scope.$digest();
+    expect(ctx.tag.showEdit).to.be.false;
   });
 });
