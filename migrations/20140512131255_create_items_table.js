fa85f3ea507fbc45028211db212a20f50c81ba55
Ben Lewis <bennlewis@gmail.com>
2014-05-12 15:13:51 -0600

    WIP hooking expenses into items

--- migrations/20140512131255_create_items_table.js
+++ migrations/20140512131255_create_items_table.js
@@ -1,11 +1,11 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('tags', function(t) {
+  return knex.schema.createTable('items', function(t) {
     t.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v1()'));
     t.json('fields');
     t.specificType('folders', 'uuid ARRAY').defaultTo('{}').notNullable();
     t.boolean('reviewed').defaultTo(false);
     t.uuid('user').references('id').inTable('users');
+    t.uuid('image').references('id').inTable('images');
 
     t.timestamps();
   });
