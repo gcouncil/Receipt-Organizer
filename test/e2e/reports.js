bc3aadf33dd7d7d3c0f6f171f49deed2e5f28bae
Ben Lewis <bennlewis@gmail.com>
2014-05-20 15:43:16 -0600

    add e2e tests for working with items in report editor view

--- test/e2e/reports.js
+++ test/e2e/reports.js
@@ -25,6 +25,7 @@ function createUserAndReports(self) {
   ]).done(function(results) {
     var user = results[0];
     self.factory.items.create({ vendor: 'Quick Left', total: 199.99 }, { user: user.id });
+    self.factory.items.create({ vendor: 'Slow Right', total: 555.55 }, { user: user.id });
   });
 
   self.page.get();
@@ -41,15 +42,34 @@ describe('Reports CRUD', function() {
   });
 
   context('create', function() {
-    it('should be created', function() {
+    beforeEach(function() {
       this.page.firstItem.$('input[type="checkbox"][selection]').click();
+      this.page.secondItem.$('input[type="checkbox"][selection]').click();
       this.page.itemToolbarCreateReport.click();
-      expect(this.page.reportEditor.isDisplayed()).to.eventually.be.true;
-      this.page.reportEditor.element(by.model('report.name')).sendKeys('New Report');
-      expect(this.page.reportEditor.element(by.repeater('item in items').row(0)).getText()).to.eventually.contain('Quick Left');
-      this.page.reportEditorSave.click();
-      expect(this.page.reportOrganizer.getText()).to.eventually.contain('New Report');
     });
+
+    it('should be created from items', function() {
+      expect(this.page.reportEditor.isDisplayed()).to.eventually.be.true;
+      this.page.reportEditor.element(by.model('report.name')).clear();
+      this.page.reportEditor.element(by.model('report.name')).sendKeys('Test Report');
+      this.page.reportEditorSave.click();
+      expect(this.page.reportOrganizer.getText()).to.eventually.contain('Test Report');
+    });
+
+    it('should remove items in the report editor', function() {
+      expect(this.page.reportEditor.element(by.repeater('item in items').row(0)).getText()).to.eventually.contain('Slow Right');
+      expect(this.page.reportEditor.element(by.repeater('item in items').row(1)).getText()).to.eventually.contain('Quick Left');
+      this.page.reportEditor.element(by.repeater('item in items').row(0)).element(by.linkText('Remove')).click();
+      expect(this.page.reportEditor.getText()).not.to.eventually.contain('Slow Right');
+      expect(this.page.reportEditor.element(by.repeater('item in items').row(0)).getText()).to.eventually.contain('Quick Left');
+    });
+
+    it('should not be saveable without items', function() {
+      this.page.reportEditor.element(by.repeater('item in items').row(0)).element(by.linkText('Remove')).click();
+      this.page.reportEditor.element(by.repeater('item in items').row(0)).element(by.linkText('Remove')).click();
+      expect(this.page.reportEditorSave.getAttribute('disabled')).to.eventually.equal('true');
+    });
+
   });
 });
 
