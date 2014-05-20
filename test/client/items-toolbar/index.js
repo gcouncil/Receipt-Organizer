243843b8c3e3caa8c81fd5587c3e8e06ea179d41
Ben Lewis <bennlewis@gmail.com>
2014-05-20 09:03:19 -0600

    add update report button and unit test

--- test/client/items-toolbar/index.js
+++ test/client/items-toolbar/index.js
@@ -1,1 +1,2 @@
 require('./items-toolbar-folder-button-directive_test');
+require('./items-toolbar-update-report-button-directive_test');
--- test/client/items-toolbar/items-toolbar-update-report-button-directive_test.js
+++ test/client/items-toolbar/items-toolbar-update-report-button-directive_test.js
@@ -1,12 +1,13 @@
 var angular = require('angular');
 var expect = require('chai').expect;
 
-describe('items toolbar folder button directive', function() {
+describe('items toolbar update report button directive', function() {
   beforeEach(function() {
     var ctx = this;
 
-    ctx.folderStorage = {
-      query: ctx.sinon.stub()
+    ctx.reportStorage = {
+      watch: ctx.sinon.stub(),
+      update: ctx.sinon.stub()
     };
 
     ctx.itemStorage = {
@@ -29,21 +30,22 @@ describe('items toolbar folder button directive', function() {
 
     ctx.selection = {
       selectedItems: [
-        { name: 'item1', folders: [] },
+        { name: 'item1', id: 7 },
       ]
     };
 
     angular.mock.module('ngMock', 'epsonreceipts.itemsToolbar', {
-      folderStorage: ctx.folderStorage,
+      reportStorage: ctx.reportStorage,
       itemStorage: ctx.itemStorage,
       notify: ctx.notify,
       $dropdown: ctx.dropdown
     });
+
     angular.mock.inject(function($rootScope, $compile) {
       ctx.scope = $rootScope.$new();
 
       ctx.compile = function() {
-        ctx.element = $compile('<items-toolbar-folder-button selection="selection"></items-toolbar-folder-button>')(ctx.scope);
+        ctx.element = $compile('<items-toolbar-update-report-button selection="selection"></items-toolbar-update-report-button>')(ctx.scope);
         ctx.scope.dropdown = ctx.dropdown();
         ctx.scope.selection = ctx.selection;
         ctx.scope.$digest();
@@ -60,9 +62,9 @@ describe('items toolbar folder button directive', function() {
   });
 
   describe('lifecycle', function() {
-    it('should ask folderStorage for the folders', function() {
+    it('should ask reportStorage for the reports', function() {
       var ctx = this;
-      expect(ctx.folderStorage.query).to.have.been.called;
+      expect(ctx.reportStorage.watch).to.have.been.called;
     });
 
     it('should destroy the dropdown on scope destroy', function() {
@@ -72,31 +74,30 @@ describe('items toolbar folder button directive', function() {
     });
   });
 
-  describe('foldering items', function() {
+  describe('adding items to reports', function() {
 
-    it('should folder the items with the selected folder', function() {
+    it('should add items to the selected report', function() {
       var ctx = this;
-      ctx.scope.dropdown.$scope.folderItems({ name: 'folder1', id: 1 });
-      expect(ctx.itemStorage.update).to.have.been.calledWith({ name: 'item1', folders: [1] });
+      ctx.scope.dropdown.$scope.addItemsToReport({ name: 'report1', id: 1 });
+      expect(ctx.reportStorage.update).to.have.been.calledWith({ name: 'report1', id: 1, items: [7] });
       ctx.scope.$digest();
-      expect(ctx.notify.success).to.have.been.calledWith('Added 1 item to folder1');
+      expect(ctx.notify.success).to.have.been.calledWith('Added 1 item to report1');
     });
 
     it('should not double folder items', function() {
       var ctx = this;
       ctx.selection = {
         selectedItems: [
-          { name: 'item2', folders: [1] },
+          { name: 'item2', id: 8 },
         ]
       };
       ctx.compile();
 
-      ctx.scope.dropdown.$scope.folderItems({ name: 'folder1', id: 1 });
-      expect(ctx.itemStorage.update).not.to.have.been.calledWith({ name: 'item2', folders: [1] });
-      expect(ctx.itemStorage.update).not.to.have.been.calledWith({ name: 'item2', folders: [1, 1] });
+      ctx.scope.dropdown.$scope.addItemsToReport({ name: 'report2', id: 2, items: [8] });
+      expect(ctx.reportStorage.update).not.to.have.been.calledWith({ name: 'report2', id: 2, items: [8, 8] });
       ctx.scope.$digest();
       expect(ctx.notify.success).not.to.have.been.called;
-      expect(ctx.notify.error).to.have.been.calledWith('Selected item already in folder1');
+      expect(ctx.notify.error).to.have.been.calledWith('Selected item already in report2');
     });
   });
 
