7f4b6f216a2cdf0afe433a7558a3987f0b0aa0cb
rrgayhart <rrgayhart@gmail.com>
2014-05-22 11:03:02 -0600

    addItem on reports handles uniq ids
    Testing and safety

--- test/domain/report_test.js
+++ test/domain/report_test.js
@@ -16,6 +16,23 @@ describe('report domain object', function() {
     expect(ctx.report.items).to.deep.equal([1]);
   });
 
+  it('should not allow duplicate items to be added to a report', function() {
+    var ctx = this;
+    expect(ctx.report.items).to.deep.equal([]);
+    ctx.report.addItem(1);
+    expect(ctx.report.items).to.deep.equal([1]);
+    ctx.report.addItem(1);
+    ctx.report.addItem(2);
+    expect(ctx.report.items).to.deep.equal([1, 2]);
+  });
+
+  it('should remove existing duplicate item ids on addItem', function() {
+    var ctx = this;
+    ctx.report2 = new domain.Report({items: [1,4,4,2]});
+    ctx.report2.addItem(3);
+    expect(ctx.report2.items).to.deep.equal([1, 2, 3, 4]);
+  });
+
   it('should remove an item', function() {
     var ctx = this;
     ctx.report.addItem(1);
