76ccadcc3b4ed13f94d87b22ead7c66e3b4316aa
rrgayhart <rrgayhart@gmail.com>
2014-05-28 15:28:48 -0600

    Strips out edit in list view functionality

--- test/client/items-list-view/items-list-view-directive_test.js
+++ test/client/items-list-view/items-list-view-directive_test.js
@@ -11,10 +11,9 @@ describe('items list view directive', function() {
       query: ctx.sinon.stub()
     };
 
-    angular.mock.module('ngMock', 'epsonreceipts.itemsListView', 'epsonreceipts.storage', {
+    angular.mock.module('ngMock', 'epsonreceipts.storage', {
       itemStorage: ctx.itemStorage,
       folderStorage: ctx.folderStorage,
-      imageStorage: ctx.imageStorage,
       options: {},
       titlecaseFilter: ctx.sinon.stub()
     });
@@ -24,7 +23,6 @@ describe('items list view directive', function() {
 
       ctx.compile = function() {
         ctx.element = $compile('<items-list-view items="items"></items-list-view>')(ctx.scope);
-        ctx.imageLoaderController = $controller('ImageLoaderController', { $scope: ctx.scope});
         ctx.scope.items = [{
           vendor: 'a',
           date: new Date(),
@@ -51,113 +49,5 @@ describe('items list view directive', function() {
     if (ctx.scope) {
       ctx.scope.$destroy();
     }
-  });
-
-//TODO Unable to test unit functionallity of image loader due to focus
-  describe('focus events', function() {
-    beforeEach(function() {
-      var ctx = this;
-      ctx.focus = function() {
-        var e =  $.Event('focus', ctx.event);
-        $(ctx.element).triggerHandler(e);
-      };
-    });
-
-    // it('set the scope item to closest row\'s item', function() {
-    //   var ctx = this;
-    //   expect(ctx.scope.item).to.be.undefined;
-    //   ctx.focus();
-    //   ctx.scope.$digest();
-    //   expect(ctx.scope.item).to.equal(ctx.scope.items[0]);
-    // });
-  });
-
-
-  describe('keydown events', function() {
-    beforeEach(function() {
-      var ctx = this;
-
-      ctx.sinon.stub($.fn, 'focus');
-
-      ctx.triggerKeydown = function() {
-        var e = $.Event('keydown', ctx.event);
-        $(ctx.element).trigger(e);
-      };
-
-      ctx.itShouldFocus = function() {
-        expect($.fn.focus).to.have.been.called;
-      };
-
-      ctx.itShouldNotFocus = function() {
-        expect($.fn.focus).not.to.have.been.called;
-      };
-
-      ctx.itShouldPreventDefault = function() {
-        expect(ctx.event.preventDefault).to.have.been.called;
-      };
-
-      ctx.itShouldNotPreventDefault = function() {
-        expect(ctx.event.preventDefault).not.to.have.been.called;
-      };
-    });
-
-    it('should work with enter key', function() {
-      var ctx = this;
-      ctx.event.which = 13;
-      ctx.triggerKeydown();
-      ctx.itShouldPreventDefault();
-      ctx.itShouldFocus();
-    });
-
-    it('should work with shift+enter key', function() {
-      var ctx = this;
-      ctx.event.shiftKey = true;
-      ctx.event.which = 13;
-      ctx.triggerKeydown();
-      ctx.itShouldPreventDefault();
-      ctx.itShouldFocus();
-    });
-
-    it('should work with ctl+left key', function() {
-      var ctx = this;
-      ctx.event.ctrlKey = true;
-      ctx.event.which = 37;
-      ctx.triggerKeydown();
-      ctx.itShouldPreventDefault();
-      ctx.itShouldFocus();
-    });
-
-    it('should work with ctl+right key', function() {
-      var ctx = this;
-      ctx.event.ctrlKey = true;
-      ctx.event.which = 39;
-      ctx.triggerKeydown();
-      ctx.itShouldPreventDefault();
-      ctx.itShouldFocus();
-    });
-
-    it('should work with up key', function() {
-      var ctx = this;
-      ctx.event.which = 38;
-      ctx.triggerKeydown();
-      ctx.itShouldPreventDefault();
-      ctx.itShouldFocus();
-    });
-
-    it('should work with down key', function() {
-      var ctx = this;
-      ctx.event.which = 40;
-      ctx.triggerKeydown();
-      ctx.itShouldPreventDefault();
-      ctx.itShouldFocus();
-    });
-
-    it('should not work with random keys', function() {
-      var ctx = this;
-      ctx.event.which = 1;
-      ctx.triggerKeydown();
-      ctx.itShouldNotPreventDefault();
-      ctx.itShouldNotFocus();
-    });
   });
 });
