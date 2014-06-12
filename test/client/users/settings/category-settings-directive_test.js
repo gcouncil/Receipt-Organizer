6612db98ba8074f1ec30a77d54bf93b893dd733b
Ben Lewis <bennlewis@gmail.com>
2014-06-12 17:18:15 -0600

    fix all the things

--- test/client/users/settings/category-settings-directive_test.js
+++ test/client/users/settings/category-settings-directive_test.js
@@ -1,5 +1,6 @@
 var angular = require('angular');
 var expect = require('chai').expect;
+var _ = require('lodash');
 
 describe('category settings directive', function() {
   beforeEach(function() {
@@ -19,7 +20,7 @@ describe('category settings directive', function() {
       go: ctx.sinon.stub()
     };
 
-    ctx.user = { id: 1, email: 'test@example.com', settings: { categories: ['category1'] } };
+    ctx.user = { id: 1, email: 'test@example.com', settings: { categories: [{ name: 'category1' }] } };
 
     ctx.currentUser = {
       get: ctx.sinon.stub().returns(ctx.user),
@@ -67,43 +68,52 @@ describe('category settings directive', function() {
   it('creates a new category', function() {
     var ctx = this;
     ctx.scope.newCategory = 'NEW CATEGORY';
-    ctx.user.settings.categories = [ 'category1', 'NEW CATEGORY' ];
+    ctx.user.settings.categories = [ { name: 'category1' } ];
     ctx.deferred.resolve(ctx.user);
     ctx.scope.$digest();
     ctx.scope.create();
     ctx.scope.$digest();
-    expect(ctx.scope.categories).to.deep.equal(ctx.user.settings.categories);
+    var categories = _.map(ctx.scope.categories, function(category) {
+      return _.omit(category, '$$hashKey');
+    });
+    expect(categories).to.deep.equal([ { name: 'category1' }, { name: 'NEW CATEGORY' } ]);
   });
 
   it('saves', function() {
     var ctx = this;
     ctx.scope.newCategory = 'NEW CATEGORY';
-    ctx.user.settings.categories = [ 'category1', 'NEW CATEGORY' ];
+    ctx.user.settings.categories = [ { name: 'category1' } ];
     ctx.deferred.resolve(ctx.user);
     ctx.scope.$digest();
     ctx.scope.saveCategories();
     ctx.scope.$digest();
-    expect(ctx.scope.categories).to.deep.equal(ctx.user.settings.categories);
+    expect(ctx.userStorage.updateSettings).to.have.been.calledWith(ctx.user);
     expect(ctx.notify.success).to.have.been.calledWith('Saved your category preferences.');
   });
 
   it('cancels preference save', function() {
     var ctx = this;
     ctx.scope.newCategory = 'NEW CATEGORY';
-    ctx.user.settings.categories = [ 'category1', 'NEW CATEGORY' ];
+    ctx.user.settings.categories = [ { name: 'category1' } ];
     ctx.deferred.resolve(ctx.user);
     ctx.scope.$digest();
     ctx.scope.cancel();
-    expect(ctx.scope.categories).not.to.deep.equal(ctx.user.settings.categories);
+    var categories = _.map(ctx.scope.categories, function(category) {
+      return _.omit(category, '$$hashKey');
+    });
+    expect(categories).to.deep.equal( [ { name: 'category1'} ] );
     expect(ctx.notify.info).to.have.been.calledWith('Your changes were cancelled.');
     expect(ctx.state.go).to.have.been.calledWith('items');
   });
 
   it('deletes', function() {
     var ctx = this;
-    ctx.scope.delete('category1');
+    ctx.scope.delete({ name: 'category1' });
     ctx.deferred.resolve(ctx.user);
     ctx.scope.$digest();
+    var categories = _.map(ctx.scope.categories, function(category) {
+      return _.omit(category, '$$hashKey');
+    });
     expect(ctx.scope.categories).to.be.empty;
     expect(ctx.notify.success).to.have.been.calledWith('Deleted the category1 category.');
   });
